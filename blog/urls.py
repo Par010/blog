ee8d689f360060a90466cbc4458f521da21ff941
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2017-08-06 10:00:42 +0530

    url mapping

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 
 from django.conf.urls import url
 from django.contrib import admin
+from posts import views as posts_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^posts/$', posts_views.post_home),
 ]
