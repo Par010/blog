38ff861f30d2febf75cc7d352ab4bdc3c1b18315
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2017-08-27 10:32:26 +0530

    detail view api

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url,include
 from .views import (
+    PostDetailAPIView,
     PostListAPIView
 )
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name="list"),
     # url(r'^create$', views.post_create),
-    # url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name="detail"),
+    url(r'^(?P<slug>[\w-]+)/$', PostDetailAPIView.as_view(), name="detail"),
     # url(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name="update"),
     # url(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete),
 ]
