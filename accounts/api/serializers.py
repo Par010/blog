178a7f9034bbc5d228892f4ee6b988d690bb089c
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2017-08-29 17:01:26 +0530

    user login api view

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import (
+    CharField,
     EmailField,
     ModelSerializer,
     HyperlinkedIdentityField,
@@ -59,3 +60,26 @@ class UserCreateSerializer(ModelSerializer):
         user_obj.set_password(password)
         user_obj.save()
         return validated_data
+
+class UserLoginSerializer(ModelSerializer):
+    token = CharField(allow_blank=True, read_only=True)
+    username = CharField()
+    email = EmailField(label='Email Address')
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'token',
+        ]
+        extra_kwargs = {
+            "password":{"write_only" : True}
+        }
+
+    # def validate(self, data):
+    #     email = data['email']
+    #     user_qs = User.objects.filter(email=email)
+    #     if user_qs.exists:
+    #         raise ValidationError("This email has already been used.")
+    #     return data
